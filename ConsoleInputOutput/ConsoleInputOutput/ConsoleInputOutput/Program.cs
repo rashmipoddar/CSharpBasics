1a631a25906592cd6e6e5dfab7c7f5ebdbfd333c
Rashmi <poddar.rashmi@gmail.com>
2020-12-20 10:25:44 -0500

    Moved number and string basics to a new folder and created a different folder for input output

---
+++ ConsoleInputOutput/ConsoleInputOutput/ConsoleInputOutput/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace ConsoleInputOutput
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
